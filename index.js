f5f3879940936a69122649abc9a6b7a02d611fe3
manoj-k-official <koppisettymanoj@gmail.com>
2024-10-05 02:37:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomDiceImg1 = "dice" + randomNumber1 + ".png";
-var randomImgSrc1 = "images/" + randomDiceImg1;
+var randomImgSrc1 = "./" + randomDiceImg1;
 document.querySelectorAll("img")[0].setAttribute("src", randomImgSrc1);
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 var randomDiceImg2 = "dice" + randomNumber2 + ".png";
-var randomImgSrc2 = "./" + randomDiceImg2;
+var randomImgSrc2 = "images/" + randomDiceImg2;
 document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
 
 if(randomNumber1 > randomNumber2){
